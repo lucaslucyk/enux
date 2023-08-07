3a5767caed10597fd6b92dfb20386671f0596f7e
Lucas Lucyk <llucyk@grupospec.com>
2023-08-07 19:21:39 -0300

    redux

--- src/frontend/enux/src/App.jsx
+++ src/frontend/enux/src/App.jsx
@@ -1,9 +1,15 @@
 import './styles/App.css'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
 
   return (
-      <h1 className="text-3xl font-bold">Vite + React</h1>
+    <Provider store={store}>
+      <div className="text-blue-600">
+        <h1 className="text-3xl font-bold">Vite + React</h1>
+      </div>
+    </Provider>
   )
 }
 
