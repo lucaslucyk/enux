5fdf11e7d9ba5edb2901ca24b69a372786c90b05
Lucas Lucyk <llucyk@grupospec.com>
2023-08-14 16:00:02 -0300

    terms and conditions

--- src/frontend/enux/src/containers/auth/Signup.jsx
+++ src/frontend/enux/src/containers/auth/Signup.jsx
@@ -143,7 +143,7 @@ const Signup = ({signup}) => {
                       className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                     />
                     <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                      Accept T&C
+                      I accept the <a href="#" className='text-blue-500'>Terms and Conditions</a>
                     </label>
                   </div>
                 </div>
